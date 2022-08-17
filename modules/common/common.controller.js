6c57a4ce3b92b40b3d83e75c16ec5a68a5a38a72
Tejas Desai <tejadesai0531@gmail.com>
2022-08-17 18:20:44 +0530

    changes

--- modules/common/common.controller.js
+++ modules/common/common.controller.js
@@ -1,9 +1,15 @@
 const restaurants = require('./restaurant.json')
+const zomatoHomePageData = require('./zomato_home.json')
 
 async function getList(req, res) {
     res.send(restaurants)
 }
 
+async function zomatoHome(req, res) {
+    res.send(zomatoHomePageData)
+}
+
 module.exports = {
-    getList
+    getList,
+    zomatoHome
 }
--- modules/common/common.route.js
+++ modules/common/common.route.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const controller = require('./common.controller')
 
 router.get('/restaurant', controller.getList)
+router.get('/zomato_home', controller.zomatoHome)
 
 
 
