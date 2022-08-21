590a1a0fb35bd342ae386d7d3b95ed93864f43b4
Tejas Desai <tejadesai0531@gmail.com>
2022-08-21 12:05:12 +0530

    Adds pagination to restaurants api

--- modules/common/common.controller.js
+++ modules/common/common.controller.js
@@ -1,8 +1,27 @@
-const restaurants = require('./restaurant.json')
 const zomatoHomePageData = require('./zomato_home.json')
 
 async function getList(req, res) {
-    res.send(restaurants)
+
+    const restaurants = require('./restaurant.json')
+
+    let page = req.query.page || 1
+    let limit = req.query.limit || 5
+
+    page = parseInt(page)
+    limit = parseInt(limit)
+
+    let start = (page - 1) * limit
+    let end = start + limit
+    let filteredRestaurants = restaurants[0].restaurants.slice(start, end)
+
+    let response = [
+        {
+            total_restaurants: restaurants[0].total_restaurants,
+            restaurants: filteredRestaurants
+        }
+    ]
+
+    res.send(response)
 }
 
 async function zomatoHome(req, res) {
